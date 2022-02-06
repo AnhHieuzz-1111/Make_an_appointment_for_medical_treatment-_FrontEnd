2381eb329559cea6b2f95359c0014323c2122f49
anhhieu.edu@gmail.com <73019698+AnhHieuzz-1111@users.noreply.github.com>
2022-02-06 23:53:36 +0700

    design modal and bootstrap

--- src/containers/System/UserManage.js
+++ src/containers/System/UserManage.js
@@ -3,11 +3,13 @@ import { FormattedMessage } from "react-intl";
 import { connect } from "react-redux";
 import "./UserManage.scss";
 import { getAllUsers } from "../../services/userService";
+import ModalUser from "./ModalUser";
 class UserManage extends Component {
   constructor(props) {
     super(props);
     this.state = {
       arrUsers: [],
+      isOpenModalUser: false,
     };
   }
 
@@ -20,6 +22,18 @@ class UserManage extends Component {
     }
   }
 
+  handleAddNewUser = () => {
+    this.setState({
+      isOpenModalUser: true,
+    })
+  };
+
+  toggleUserModal = () => {
+    this.setState({
+      isOpenModalUser: !this.state.isOpenModalUser,
+    })
+  }
+
   /*******Life cycle
    * Run component:
    * 1. Run construct -> init state
@@ -30,7 +44,19 @@ class UserManage extends Component {
     let arrUsers = this.state.arrUsers;
     return (
       <div className="users-container">
+        <ModalUser
+          isOpen = {this.state.isOpenModalUser}
+          toggleFromParent = {this.toggleUserModal}
+        />
         <div className="title text-center mt-4">Manage users with Anh Hieu</div>
+        <div className="mx-1-">
+          <button
+            className="btn btn-primary px-3 mx-2"
+            onClick={() => this.handleAddNewUser()}
+          >
+            <i class="fas fa-user-plus"></i> Add new user
+          </button>
+        </div>
         <div className="users-table mt-3 mx-2">
           <table id="customers">
             <tr>
@@ -40,21 +66,25 @@ class UserManage extends Component {
               <th>Address</th>
               <th>Actions</th>
             </tr>
-              {arrUsers &&
-                arrUsers.map((item, index) => {
-                  return (
-                    <tr>
-                      <td>{item.email}</td>
-                      <td>{item.firstName}</td>
-                      <td>{item.lastName}</td>
-                      <td>{item.address}</td>
-                      <td>
-                          <button className="btn-edit"><i className="fas fa-edit"></i></button>
-                          <button className="btn-delete"><i class="fas fa-trash-alt"></i></button>
-                      </td>
-                    </tr>
-                  );
-                })}
+            {arrUsers &&
+              arrUsers.map((item, index) => {
+                return (
+                  <tr key={index}>
+                    <td>{item.email}</td>
+                    <td>{item.firstName}</td>
+                    <td>{item.lastName}</td>
+                    <td>{item.address}</td>
+                    <td>
+                      <button className="btn-edit">
+                        <i className="fas fa-edit"></i>
+                      </button>
+                      <button className="btn-delete">
+                        <i class="fas fa-trash-alt"></i>
+                      </button>
+                    </td>
+                  </tr>
+                );
+              })}
           </table>
         </div>
       </div>
