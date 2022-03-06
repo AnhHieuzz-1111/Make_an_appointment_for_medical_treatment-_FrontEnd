8ebb729b13fa159498b86ae80ab1f1c2345ad089
anhhieu.edu@gmail.com <73019698+AnhHieuzz-1111@users.noreply.github.com>
2022-03-06 22:28:53 +0700

    embed library react-markdown-editor to admin

--- src/containers/System/Admin/TableManageUser.js
+++ src/containers/System/Admin/TableManageUser.js
@@ -3,6 +3,21 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import "./TableManageUser.scss";
 import * as actions from "../../../store/actions";
+import MarkdownIt from "markdown-it";
+import MdEditor from "react-markdown-editor-lite";
+// import style manually
+import "react-markdown-editor-lite/lib/index.css";
+
+// Register plugins if required
+// MdEditor.use(YOUR_PLUGINS_HERE);
+
+// Initialize a markdown parser
+const mdParser = new MarkdownIt(/* Markdown-it options */);
+
+// Finish!
+function handleEditorChange({ html, text }) {
+  console.log("handleEditorChange", html, text);
+}
 
 class TableManageUser extends Component {
   constructor(props) {
@@ -25,51 +40,60 @@ class TableManageUser extends Component {
   }
 
   handleDeleteUser = (user) => {
-      this.props.deleteUserRedux(user.id);
-  }
+    this.props.deleteUserRedux(user.id);
+  };
 
   handleEditUser = (user) => {
-    this.props.handleEditUserFromParentKey(user)
-  }
+    this.props.handleEditUserFromParentKey(user);
+  };
 
   render() {
     let usersArr = this.state.usersRedux;
     return (
-      <table id="TableManageUser">
-        <tbody>
-          <tr>
-            <th>Email</th>
-            <th>First name</th>
-            <th>Last name</th>
-            <th>Address</th>
-            <th>Actions</th>
-          </tr>
-          {usersArr &&
-            usersArr.length > 0 &&
-            usersArr.map((item, index) => {
-              return (
-                <tr key={index}>
-                  <td>{item.email}</td>
-                  <td>{item.firstName}</td>
-                  <td>{item.lastName}</td>
-                  <td>{item.address}</td>
-                  <td>
-                    <button 
-                        onClick={() => this.handleEditUser(item) }
-                        className="btn-edit">
-                      <i className="fas fa-edit"></i>
-                    </button>
-                    <button 
+      <React.Fragment>
+        <table id="TableManageUser">
+          <tbody>
+            <tr>
+              <th>Email</th>
+              <th>First name</th>
+              <th>Last name</th>
+              <th>Address</th>
+              <th>Actions</th>
+            </tr>
+            {usersArr &&
+              usersArr.length > 0 &&
+              usersArr.map((item, index) => {
+                return (
+                  <tr key={index}>
+                    <td>{item.email}</td>
+                    <td>{item.firstName}</td>
+                    <td>{item.lastName}</td>
+                    <td>{item.address}</td>
+                    <td>
+                      <button
+                        onClick={() => this.handleEditUser(item)}
+                        className="btn-edit"
+                      >
+                        <i className="fas fa-edit"></i>
+                      </button>
+                      <button
                         onClick={() => this.handleDeleteUser(item)}
-                    className="btn-delete">
-                      <i className="fas fa-trash-alt"></i>
-                    </button>
-                  </td>
-                </tr>
-              );
-            })}
-        </tbody>
-      </table>
+                        className="btn-delete"
+                      >
+                        <i className="fas fa-trash-alt"></i>
+                      </button>
+                    </td>
+                  </tr>
+                );
+              })}
+          </tbody>
+        </table>
+        <MdEditor
+          style={{ height: "500px" }}
+          renderHTML={(text) => mdParser.render(text)}
+          onChange={handleEditorChange}
+        />
+      </React.Fragment>
     );
   }
 }
@@ -83,7 +107,7 @@ const mapStateToProps = (state) => {
 const mapDispatchToProps = (dispatch) => {
   return {
     fetchAllUserRedux: () => dispatch(actions.fetchAllUserStart()),
-    deleteUserRedux: (id) =>  dispatch(actions.deleteUser(id))
+    deleteUserRedux: (id) => dispatch(actions.deleteUser(id)),
   };
 };
 
